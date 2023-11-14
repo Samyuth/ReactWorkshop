cf60494f294c429a1d558590b238a953ecd4820e
Samyuth <samyuth.sagi@gmail.com>
2023-11-14 16:22:49 -0500

    Adding a basic component

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,18 @@
+function Clicker() {
+  return (
+    <>
+      <button>Click Me</button>
+    </>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      hi
+      <h1>The clickers have assembled</h1>
+      <Clicker />
+      <Clicker />
+      <Clicker />
     </div>
   );
 }
